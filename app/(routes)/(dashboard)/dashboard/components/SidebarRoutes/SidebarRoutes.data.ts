f675965db00b5519d132a28c807d15a563c09cc4
Ivan Nj <ivannhrro@gmail.com>
2025-01-02 05:02:34 +0100

    Creating info inside button Add new Car + created resources.txt with important info about resources

--- app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.data.ts
+++ app/(routes)/(dashboard)/dashboard/components/SidebarRoutes/SidebarRoutes.data.ts
@@ -24,7 +24,7 @@ export const dataAdminSidebar = [
     {
         icon: SquareChartGantt,
         label: "Manage your cars",
-        href: "/cars-manager",
+        href: "/dashboard/admin/cars-manager",
     },
 
     {
