9160553d8cbc7a7eda0e5cd062f503d4c02f741e
Ivan Nj <ivannhrro@gmail.com>
2025-01-23 06:29:28 +0100

    Fix button close in formAddCar

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ButtonAddCar/ButtonAddCar.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/components/ButtonAddCar/ButtonAddCar.tsx
@@ -14,23 +14,24 @@ import { FormAddCar } from "../FormAddCar"
   
 
 export function ButtonAddCar() {
-    const [openDialog, setOpenDialog] = useState(false)
-  return (
-    <Dialog open={openDialog}>
+    const [openDialog, setOpenDialog] = useState(false);
+  
+    return (
+      <Dialog open={openDialog} onOpenChange={setOpenDialog}>
         <DialogTrigger asChild>
-            <Button variant="outline" onClick={() => setOpenDialog(true)}>
-                Add new car
-                <PlusCircle className="ml-2" />
-            </Button>
+          <Button variant="outline" onClick={() => setOpenDialog(true)}>
+            Add new car
+            <PlusCircle className="ml-2" />
+          </Button>
         </DialogTrigger>
         <DialogContent className="w-full mx-auto p-4 max-h-[90vh] overflow-y-auto">
-            <DialogHeader>
+          <DialogHeader>
             <DialogTitle>Add a New Car</DialogTitle>
-                <DialogDescription>
-                    <FormAddCar setOpenDialog={setOpenDialog}/>
-                </DialogDescription>
-            </DialogHeader>
+            <DialogDescription>
+              <FormAddCar setOpenDialog={setOpenDialog} />
+            </DialogDescription>
+          </DialogHeader>
         </DialogContent>
-    </Dialog>
-  )
-}
+      </Dialog>
+    );
+  }
