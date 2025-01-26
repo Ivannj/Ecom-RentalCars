746c799c63ad9b7771db5ac027c5a5da303943c3
Ivan Nj <ivannhrro@gmail.com>
2025-01-26 13:50:23 +0100

    Calendar Issues fix/done

--- components/Shared/ModalAddReservation/CalendarSelector/CalendarSelector.tsx
+++ components/Shared/ModalAddReservation/CalendarSelector/CalendarSelector.tsx
@@ -5,84 +5,100 @@ import { addDays, format } from "date-fns";
 import { DateRange } from "react-day-picker";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { CalendarSelectorProps } from "./CalendarSelector.types";
 import { useEffect, useState } from "react";
-import { cn } from "@/lib/utils";
 
-export  function CalendarSelector(props: CalendarSelectorProps) {
-    const {setDateSelected, className, carPriceDay} = props;
-    const [date, setDate] = useState<DateRange | undefined>({
-        from: new Date,
-        to: addDays(new Date(), 5)
-    })
+export function CalendarSelector(props: CalendarSelectorProps) {
+  const { setDateSelected, className, carPriceDay } = props;
+  const [date, setDate] = useState<DateRange | undefined>({
+    from: new Date(),
+    to: addDays(new Date(), 5),
+  });
 
-    useEffect(() => {
+  const [numberOfMonths, setNumberOfMonths] = useState(1);
 
-        setDateSelected({
-            from: date?.from,
-            to: date?.to,
-        })
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 1024) {
+        setNumberOfMonths(2); // Laptops y pantallas grandes
+      } else {
+        setNumberOfMonths(1); // Tablets y móviles
+      }
+    };
 
+    handleResize();
+    window.addEventListener("resize", handleResize);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [date]);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    const calculateDaysBetween = (from: Date, to:Date): number => {
-        const oneDay= 24 * 60 * 60 * 1000;
-        const diffInTime = to.getTime() - from.getTime()
-        return Math.max(1, Math.round(diffInTime / oneDay));
-    }
+  useEffect(() => {
+    setDateSelected({
+      from: date?.from,
+      to: date?.to,
+    });
+  }, [date, setDateSelected]);
 
-    const daysBetween = date?.from && date?.to ? calculateDaysBetween(date.from, date.to) : 0;
+  const calculateDaysBetween = (from: Date, to: Date): number => {
+    const oneDay = 24 * 60 * 60 * 1000;
+    const diffInTime = to.getTime() - from.getTime();
+    return Math.max(1, Math.round(diffInTime / oneDay));
+  };
 
-    
+  const daysBetween =
+    date?.from && date?.to ? calculateDaysBetween(date.from, date.to) : 0;
+
   return (
-    <> 
-    <div className={cn("grid gap-2", className)}>
-        {date?.from && date?.to && (
-            <>
-            <p className="mt-4 text-lg text-black">
-                Total Days {daysBetween} 
-                </p>
-                <p className="mb-4 text-md">
-                    Total Price: {daysBetween * Number(carPriceDay)}$ (Tax Included)
-                </p>
-            </>
-        )}
-        <Popover>
-            <PopoverTrigger asChild>
-            <Button id="date" variant="outline" className={cn("justify-start text-left font-normal", !date && "text-muted-foreground")}>
-
-                <CalendarIcon className="w-4 h-4 mr-2" /> 
-
-                {date?.from ? (
-                    date.to ? (
-                        <>
-                        {format(date.from, "LLL dd, y")} - {""}
-                        {format(date.to, "LLL dd, y")}                        
-                        </>
-                    ) : (
-                        format(date.from, "LLL dd,y")
-                    )
-                ) : (
-                    <span> Pick a date</span>
-                )}
-                
-            </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-                <Calendar 
-                initialFocus
-                mode="range"
-                defaultMonth={date?.from}
-                selected={date}
-                onSelect={setDate}
-                numberOfMonths={2} />
-            </PopoverContent>
-        </Popover>
-
+    <div className={className}>
+      {date?.from && date?.to && (
+        <>
+          <p className="mt-4 text-lg text-black">Total Days {daysBetween}</p>
+          <p className="mb-4 text-md">
+            Total Price:{" "}
+            {parseFloat((daysBetween * Number(carPriceDay)).toFixed(2))}$ (Tax
+            Included)
+          </p>
+        </>
+      )}
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant="outline"
+            className="justify-start text-left font-normal"
+          >
+            <CalendarIcon className="w-4 h-4 mr-2" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd,y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-full max-w-[700px] p-0">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={numberOfMonths}
+            className="max-w-full"
+          />
+        </PopoverContent>
+      </Popover>
     </div>
-    </>
-  )
+  );
 }
